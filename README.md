1599f2851a0866e2aca1b4843b721a4aeedd4f36
unknown <dennyssistema@gmail.com>
2024-06-23 15:07:05 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,15 @@
+## Requisitos
+* PHP 8.2
+* Composer
+
+## Comando para criar um projeto laravel na pasta raiz
+composer create-project laravel/laravel:^11.0 .
+
+## Comando para iniciar o projeto / starta o server
+php artisan serve
+
+## Instalar dependências do php
+composer install
+
+## Geração de key variável de ambiente
+php artisan key:generate
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Curso</title>
+       
+    </head>
+    <body>
+        <h1>Curso de Laravel 11</h1>
+        {{-- <p>Data atual: {{ \Carbon\Carbon::now()->format('d/m/Y H:i:s') }}</p> --}}
+    </body>
+</html>
